396661ee13fece82c3ea6aeb4fb81344d97e6189
keeppeng <keeppeng@foxmail.com>
2022-04-20 23:07:14 +0800

    new add about No.70

---
+++ src/com/keeppeng/lc/ClimbStairsTest.java
@@ -0,0 +1,13 @@
+package com.keeppeng.lc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClimbStairsTest {
+private ClimbStairs instance ;
+    @Test
+    public void climbStairs1() {
+        assertEquals(3,instance);
+    }
+}
